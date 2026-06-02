b69c1cc09e7fb1fe2e487a68492a12cec4c02182
kjancheta <kaikenjancheta04@gmail.com>
2026-06-02 07:49:44 +0000

    readsuperblock, readinodebitmap, readdatabitmap in localfilesystem done

--- project4/gunrock_web/LocalFileSystem.cpp
+++ project4/gunrock_web/LocalFileSystem.cpp
@@ -2,31 +2,50 @@
 #include <string>
 #include <vector>
 #include <assert.h>
+#include <string.h>
+#include <cstring>
+#include <algorithm>
+#include <iomanip>
+#include <sstream>
 
 #include "LocalFileSystem.h"
 #include "ufs.h"
 
 using namespace std;
-//
+
+
+// member functions
 
 LocalFileSystem::LocalFileSystem(Disk *disk) {
   this->disk = disk;
 }
 
-void LocalFileSystem::readSuperBlock(super_t *super) {
-
+void LocalFileSystem::readSuperBlock(super_t *super) { 
+  unsigned char readBuffer[UFS_BLOCK_SIZE]; // hold 1 disk block
+  disk->readBlock(0, readBuffer); // super block at 0 on disk
+  memcpy(super, readBuffer, sizeof(super_t)); // copy bytes of readbuffer to super
 }
 
-void LocalFileSystem::readInodeBitmap(super_t *super, unsigned char *inodeBitmap) {
+void LocalFileSystem::readInodeBitmap(super_t *super, unsigned char *inodeBitmap) { 
+  unsigned char tempBuffer[UFS_BLOCK_SIZE]; // hold 1 disk block
 
+  for (int i = 0; i < super->inode_bitmap_len; i++) { // loop through every disk block for inode bitmap
+    disk->readBlock(super->inode_bitmap_addr + i, tempBuffer); // read current block, starting at base and adding i block offset
+    memcpy(inodeBitmap + (i * UFS_BLOCK_SIZE), tempBuffer, UFS_BLOCK_SIZE); // copy block into inodebitmap, offset by i*4096 bytes
+  }
 }
 
 void LocalFileSystem::writeInodeBitmap(super_t *super, unsigned char *inodeBitmap) {
 
 }
 
-void LocalFileSystem::readDataBitmap(super_t *super, unsigned char *dataBitmap) {
+void LocalFileSystem::readDataBitmap(super_t *super, unsigned char *dataBitmap) { 
+  unsigned char tempBuffer[UFS_BLOCK_SIZE]; // hold 1 disk block
 
+  for (int i = 0; i < super->data_bitmap_len; i++) { // loop through every disk block for data bitmap
+    disk->readBlock(super->data_bitmap_addr + i, tempBuffer); // read current block, starting at base and adding i block offset
+    memcpy(dataBitmap + (i * UFS_BLOCK_SIZE), tempBuffer, UFS_BLOCK_SIZE); // copy block into inodebitmap, offset by i*4096 bytes
+  }
 }
 
 void LocalFileSystem::writeDataBitmap(super_t *super, unsigned char *dataBitmap) {
