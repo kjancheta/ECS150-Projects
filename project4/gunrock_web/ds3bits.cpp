2f06b82a91ca67241e137ae4aedd915234e2e70c
kjancheta <kaikenjancheta04@gmail.com>
2026-06-02 08:06:20 +0000

    ds3bits.cpp done

--- project4/gunrock_web/ds3bits.cpp
+++ project4/gunrock_web/ds3bits.cpp
@@ -17,10 +17,42 @@ int main(int argc, char *argv[]) {
   }
 
   // Parse command line arguments
-  /*
+  
   Disk *disk = new Disk(argv[1], UFS_BLOCK_SIZE);
   LocalFileSystem *fileSystem = new LocalFileSystem(disk);
-  */
+
+  super_t* super = new super_t;
+  fileSystem->readSuperBlock(super);
+
+  cout << "Super" << endl;
+  cout << "inode_region_addr " << super->inode_region_addr << endl;
+  cout << "inode_region_len " << super->inode_region_len << endl; 
+  cout << "num_inodes " << super->num_inodes << endl;
+  cout << "data_region_addr " << super->data_region_addr << endl;
+  cout << "data_region_len " << super->data_region_len << endl;
+  cout << "num_data " << super->num_data << endl;
+  cout << endl;
+
+  // inode bitmap
+  unsigned char inodeBitmap[(super->inode_bitmap_len) * UFS_BLOCK_SIZE]; // to hold inodebitmap region
+  fileSystem->readInodeBitmap(super, inodeBitmap); // read bitmap data
+  cout << "Inode bitmap" << endl;
+  for (int i = 0; i < (super->num_inodes)/8; i++) { // loop and print bitmap btyes
+    cout << (unsigned int) inodeBitmap[i] << " ";
+  }
+  cout << endl << endl;
+  // data bitmap
+  unsigned char dataBitmap[(super->data_bitmap_len) * UFS_BLOCK_SIZE]; // to hold databitmap region
+  fileSystem->readDataBitmap(super, dataBitmap); // read bitmap data
+  cout << "Data bitmap" << endl;
+  for (int i = 0; i < (super->num_data)/8; i++) { // loop and print bitmap btyes
+    cout << (unsigned int) dataBitmap[i] << " ";
+  }
+  cout << endl;
+
+  delete super;
+  delete fileSystem;
+  delete disk;
   
   return 0;
 }
